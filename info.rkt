5091e827770713a6436fd56790742c576b188926
Maciej Barć <xgqt@gentoo.org>
2022-12-06 06:23:24 +0100

    info.rkt: add license definition
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- info.rkt
+++ info.rkt
@@ -5,6 +5,7 @@
 (define pkg-desc "Generate random UUIDs in pure Racket")
 (define version "0.1")
 (define pkg-authors '(philip))
+(define license 'Apache-2.0)
 
 (define deps '(["base" #:version "7.6"]))
 (define build-deps '("scribble-lib"
